70f9bcb9866d6ae907c5445f978b8d0f9be8cfa9
potatofinder <137095289+potatofinder@users.noreply.github.com>
2024-05-07 19:03:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sendToDiscord() {
   var request = new XMLHttpRequest();
-  request.open("POST", "YOUR_WEBHOOK_URL");
+  request.open("POST", "https://discord.com/api/webhooks/1237584893756575845/AP_2URcUznCp86IZ2GlJeJxNfyr6TXKxhA1wjU7YZvh-SKXSK6OuEaBvWvoqC6zDcfBO");
 
   request.setRequestHeader('Content-type', 'application/json');
 
